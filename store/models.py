b855d6db55a26b43c7bbbf9f78d66508d4307d3f
Sina Arasteh <sinna.arasteh@gmail.com>
2025-07-09 14:11:09 +0330

    Add the 'Category' model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
