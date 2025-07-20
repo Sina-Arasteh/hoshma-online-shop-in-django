8489b0c0e8bb45687ca4596147fd71e2c13ce09a
Sina Arasteh <sinna.arasteh@gmail.com>
2025-07-20 15:15:29 +0330

    Register all models in the admin site

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.apps import apps
+from .models import Customer
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+
+models = apps.get_models()
+for model in models:
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
+
+
+admin.site.unregister(User)
+admin.site.unregister(Customer)
+
+
+class CustomerInline(admin.StackedInline):
+    model = Customer
+    can_delete = False
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    inlines = [Customer]
