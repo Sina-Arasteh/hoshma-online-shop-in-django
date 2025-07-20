012ff04e7a2850a244918664083d4e0597c8f69c
Sina Arasteh <sinna.arasteh@gmail.com>
2025-07-20 19:38:18 +0330

    Set verbose_name for the apps

--- account/apps.py
+++ account/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class AccountConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'account'
+    verbose_name = "حساب کاربری"
 
     def ready(self):
         from . import signals
