d92c80ce17e3057a94e1e39f628c7b06b7ed2eb4
Sina Arasteh <sinna.arasteh@gmail.com>
2025-08-05 16:28:41 +0330

    Update the 'CategorySerializer' serializer

--- store/serializers.py
+++ store/serializers.py
@@ -3,13 +3,31 @@ from . import models
 
 
 class CategorySerializer(serializers.ModelSerializer):
-    parent = serializers.StringRelatedField()
+    parent_read = serializers.StringRelatedField()
+    parent_write = serializers.PrimaryKeyRelatedField(
+        queryset=models.Category.objects.all(),
+        source="parent",
+        write_only=True
+    )
+
     class Meta:
         model = models.Category
-        fields = "name, parent"
+        fields = "id, name, parent_read, parent_write"
+    
+    def validate(self, data):
+        instance = models.Category(**data)
+        if self.instance:
+            instance.id = self.instance.id
+        instance.clean()
+        return data
 
 
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Product
-        
+
+class DiscountSerializer(serializers.ModelSerializer):
+    pass
+    
+
+
+# class ProductSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = models.Product
