cf45da411f912d4314d7996d65698b1783886f41
Sina Arasteh <sinna.arasteh@gmail.com>
2025-08-06 12:19:19 +0330

    Set up the 'Discount' model endpoints

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 app_name = "store"
 urlpatterns = [
-    path('categories/', views.CategoryListAPIView.as_view(), name='categories'),
-    path('categories/<int:pk>/', views.CategoryDetailAPIView.as_view(), name='category'),
+    path('categories/', views.CategoryListAPIView.as_view(), name='category-list'),
+    path('categories/<int:pk>/', views.CategoryDetailAPIView.as_view(), name='category-detail'),
+    path('discounts/', views.CategoryListAPIView.as_view(), name='discount-list'),
+    path('discounts/<int:pk>/', views.CategoryDetailAPIView.as_view(), name='discount-detail'),
 ]
--- store/views.py
+++ store/views.py
@@ -9,7 +9,7 @@ class CategoryListAPIView(APIView):
     def get(self, request):
         categories = models.Category.objects.all()
         serializer = serializers.CategorySerializer(categories, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data)
     
     def post(self, request):
         serializer = serializers.CategorySerializer(data=request.data)
@@ -29,17 +29,57 @@ class CategoryDetailAPIView(APIView):
     def get(self, request, pk):
         category = self.get_object(pk)
         serializer = serializers.CategorySerializer(category)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data)
     
     def put(self, request, pk):
         category = self.get_object(pk)
         serializer = serializers.CategorySerializer(category, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     def delete(self, request, pk):
         category = self.get_object(pk)
         category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class DiscountListAPIView(APIView):
+    def get(self, request):
+        discounts = models.Discount.objects.all()
+        serializer = serializers.DiscountSerializer(request.data)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = serializers.DiscountSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DiscountDetailAPIView(APIView):
+    def get_object(self, pk):
+        try:
+            return models.Discount.objects.get(pk=pk)
+        except models.Discount.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        discount = self.get_object(pk)
+        serializer = serializers.DiscountSerializer(discount)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        discount = self.get_object(pk)
+        serializer = serializers.DiscountSerializer(discount, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        discount = self.get_object(pk)
+        discount.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
