29634de13850ed32c6687a8cd241e13414081d97
Sina Arasteh <sinna.arasteh@gmail.com>
2025-08-18 18:23:05 +0330

    Add some templates

--- config/settings.py
+++ config/settings.py
@@ -46,7 +46,9 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "templates",
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,6 +109,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "collected_static_files"
 
 
 # Default primary key field type
